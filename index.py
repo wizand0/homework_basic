3b0ff3b3aef333f467ae80191ce3aaa4c0367bf4
Makarov Andrey <xabor@yandex.ru>
2022-01-17 23:12:18 +0300

    Исправление ошибки Поинт 4 #5

--- index.py
+++ index.py
@@ -37,7 +37,7 @@ for user in users:
     friends = user.get('friends', [])
     for friend in friends:
         sum_of_salaries += friend['job']['salary']
-    if sum_of_salaries > max_friends_total_salary:
+    if sum_of_salaries > vip_sum_of_salaries:
         vip_sum_of_salaries = sum_of_salaries
         vip_user = user['name']
     
@@ -49,27 +49,27 @@ for user in users:
 #            vip_sum_of_salaries = sum_of_salaries
 #            vip_user = user['name']
 
-# Point 5: Путешественники.
-            if 'flights' in friend and 'cars' in friend:
-                count_friends_with_cars += 1
-                print(len(friend['flights']))
-                count_flights_by_friends_with_cars += len(friend['flights'])
-            try:
-                avg_flights = round(count_flights_by_friends_with_cars/count_friends_with_cars, 5)
-            except ZeroDivisionError:
-                print('У кого то есть ключ flights но он пустой')
+# # Point 5: Путешественники.
+#             if 'flights' in friend and 'cars' in friend:
+#                 count_friends_with_cars += 1
+#                 print(len(friend['flights']))
+#                 count_flights_by_friends_with_cars += len(friend['flights'])
+#             try:
+#                 avg_flights = round(count_flights_by_friends_with_cars/count_friends_with_cars, 5)
+#             except ZeroDivisionError:
+#                 print('У кого то есть ключ flights но он пустой')
             
 
-# Point 6: Чистка списка
+# # Point 6: Чистка списка
             
-            if 'flights' in friend:
-                for country in friend['flights']:
-                    if country['country'] in countries:
-                        to_delete_list.append(user['name'])
+#             if 'flights' in friend:
+#                 for country in friend['flights']:
+#                     if country['country'] in countries:
+#                         to_delete_list.append(user['name'])
 
-to_delete_list = list(set(to_delete_list))
+# to_delete_list = list(set(to_delete_list))
 
 
-for user in users:
-    if user['name'] in to_delete_list:
-        users.remove(user)
+# for user in users:
+#     if user['name'] in to_delete_list:
+#         users.remove(user)
