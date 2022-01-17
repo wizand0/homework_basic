d7e83a3f2196fddb916361f1e562406db07dec83
Makarov Andrey <xabor@yandex.ru>
2022-01-17 10:21:43 +0300

    Initial plus Point 1 - #3

--- index.py
+++ index.py
@@ -9,14 +9,10 @@ from pprint import pprint
 #print(type(users['password']))
 users_wrong_password = [] # Создание пустого словаря
 
-for user in users:
-    if user['password'].isdigit():
-        dict1 = {user['name']: user['mail']}
-        users_wrong_password.append(dict1)
-
-
-    else:
-        print('Good password')
-
-print(users_wrong_password)
-#    print(user['password'])
+try:
+    for user in users:
+        if user['password'].isdigit():
+            dict1 = {user['name']: user['mail']}
+            users_wrong_password.append(dict1)
+except KeyError:
+    print('Нет ключа: name')
